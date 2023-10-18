db0b1be42ab220fc0056192a47ec4036653d23a4
Ruslan Ismailov <rusishss@mail.ru>
2023-10-18 10:59:47 +0600

    added postLikes entity

--- src/likes/db.likes.types.ts
+++ src/likes/db.likes.types.ts
@@ -1,3 +1,30 @@
+import { Posts } from "src/posts/posts.types";
+import { Users } from "src/users/users.types";
+import { Column, Entity, JoinColumn, OneToOne, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class PostLikes {
+    @PrimaryGeneratedColumn('uuid')
+    postLikeId: string
+    @OneToOne(()=>Posts)
+    @JoinColumn({name: 'postId'})
+    Posts: Posts
+    @Column('uuid')
+    postId: string;
+    @Column()
+    addedAt: string;
+    @OneToOne(() => Users )
+    @JoinColumn({ name: 'userId' })
+    Users: Users
+    @Column('uuid')
+    userId: string;
+    @Column()
+    login: string;
+    @Column()
+    isUserBanned: boolean;
+    @Column()
+    status: string;
+}
 
 export class PostLikeDbType {
     constructor(
