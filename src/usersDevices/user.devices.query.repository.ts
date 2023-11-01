d8a3b71e058668ac8f57ce6baa9bdd81126c901c
Ruslan <rusishss@mail.ru>
2023-11-01 15:49:18 +0600

    getActiveUserDevices to typeorm

--- src/usersDevices/user.devices.query.repository.ts
+++ src/usersDevices/user.devices.query.repository.ts
@@ -1,21 +1,28 @@
 import { Injectable } from '@nestjs/common';
-import { InjectDataSource } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
+import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { validate as isValidUUID } from 'uuid';
+import { UserDevices } from './users-devices.types';
 
 @Injectable()
 export class UserDevicesQueryRepository {
-  constructor(@InjectDataSource() protected dataSource: DataSource) {}
+  constructor(@InjectDataSource() protected dataSource: DataSource,
+              @InjectRepository(UserDevices) private readonly userDevicesRepository: Repository<UserDevices> ) {}
 
   async getActiveUserDevices(userId: string) {
     if (!isValidUUID(userId)) {
       return null;
     }
-    const query = `
-    SELECT ip, title, "lastActiveDate", "deviceId"
-    FROM public."UserDevices"
-    WHERE "userId" = $1`;
-    const result = await this.dataSource.query(query, [userId]);
+    const result = await this.userDevicesRepository
+    .createQueryBuilder('device')
+    .select([
+      "device.ip",
+      "device.title",
+      "device.lastActiveDate",
+      "device.deviceId"
+    ])
+    .where("device.userId = :userId", {userId: userId})
+    .getMany();
     return result;
   }
 }
