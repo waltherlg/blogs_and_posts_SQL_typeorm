34661071d973747bf2244bc7210c711cc95b6682
Ruslan Ismailov <rusishss@mail.ru>
2023-11-06 00:17:12 +0600

    trying set datasource for migration

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -1,4 +1,10 @@
 import { DataSource } from "typeorm";
+import { Users } from "../users/user.entity";
+import { UserDevices } from "../usersDevices/user.device.entity";
+import { Blogs } from "../blogs/blog.entity";
+import { Posts } from "../posts/post.entity";
+import { Comments } from "../comments/comment.entity";
+import { CommentLikes, PostLikes } from "../likes/like.entity";
 
 export default new DataSource({
         type: 'postgres',
@@ -8,6 +14,6 @@ export default new DataSource({
         password: 'nest',
         database: 'blogs_and_posts_typeorm',
         synchronize: false,
-        entities: ['src/**/*.entity{.ts,.js}']
+        entities: [Users, UserDevices, Blogs, Posts, Comments, PostLikes, CommentLikes]
       }
 )
