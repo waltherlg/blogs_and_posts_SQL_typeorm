7ebc076bb7d46f94f490652c5b7aa8ff60bd865f
Ruslan Ismailov <rusishss@mail.ru>
2023-11-05 19:45:02 +0600

    put the comment entity in a single file

--- src/comments/comments.types.ts
+++ src/comments/comments.types.ts
@@ -2,30 +2,6 @@ import { Posts } from '../posts/posts.types';
 import { Users } from '../users/users.types';
 import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
 
-@Entity({ name: 'Comments' })
-export class Comments {
-  @PrimaryColumn()
-  commentId: string;
-  @ManyToOne(() => Posts, (p) => p.Comments)
-  @JoinColumn({ name: 'postId' })
-  Posts: Posts;
-  @Column('uuid')
-  postId: string;
-  @Column()
-  content: string;
-  @Column()
-  createdAt: string;
-  @ManyToOne(() => Users, (u) => u.Comments)
-  @JoinColumn({ name: 'userId' })
-  Users: Users;
-  @Column('uuid')
-  userId: string;
-  @Column()
-  likesCount: number;
-  @Column()
-  dislikesCount: number;
-}
-
 export class CommentDBType {
   constructor(
     public commentId: string,
