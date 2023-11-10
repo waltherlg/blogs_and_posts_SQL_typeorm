f23e8a0c68cf6b47f51b3f49d64862c2d8bc87e0
Ruslan Ismailov <rusishss@mail.ru>
2023-11-10 21:55:03 +0600

    updateCommentById to typeorm

--- src/comments/comments.repository.ts
+++ src/comments/comments.repository.ts
@@ -43,13 +43,10 @@ export class CommentsRepository {
   }
 
   async updateCommentById(commentId, content): Promise<boolean> {
-    const query = `
-    UPDATE public."Comments"
-    SET "content" = $2
-    WHERE "commentId" = $1
-    `;
-    const result = await this.dataSource.query(query, [commentId, content]);
-    const count = result[1];
-    return count === 1;
+    const result = await this.commentsRepository.update(
+      {commentId: commentId},
+      {content: content}
+    )
+    return result.affected > 0
   }
 }
