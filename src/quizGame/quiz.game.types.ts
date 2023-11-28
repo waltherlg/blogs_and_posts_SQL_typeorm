103ffd4c5245cb7caad72f70180bf405f0354850
Ruslan Ismailov <rusishss@mail.ru>
2023-11-28 12:30:36 +0600

    create Questions entity

--- src/quizGame/quiz.game.types.ts
+++ src/quizGame/quiz.game.types.ts
@@ -1,5 +1,6 @@
 import { IsArray, IsBoolean, Length } from "class-validator";
 import { StringTrimNotEmpty } from "../middlewares/validators";
+import { Entity, PrimaryColumn, Column } from "typeorm";
 
 export class CreateQuestionImputModelType {
     @StringTrimNotEmpty()
@@ -31,5 +32,20 @@ export class QuestionDbType {
         public createdAt: string,
         public updatedAt: string | null,
     ) {}
+}
 
+@Entity({name: 'Questions'})
+export class Questions {
+    @PrimaryColumn('uuid')
+    questionId: string;
+    @Column()
+    body: string;
+    @Column({ type: 'jsonb' })
+    correctAnswers: string[];
+    @Column()
+    published: boolean;
+    @Column({ type: 'timestamptz'})
+    createdAt: string;
+    @Column({ type: 'timestamptz', nullable: true })
+    updatedAt: string | null;    
 }
