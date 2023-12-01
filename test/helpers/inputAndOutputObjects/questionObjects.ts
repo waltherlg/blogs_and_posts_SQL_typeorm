b6f60a5cbe2e796ed6204cc4fe62d36a3eff699f
Ruslan <rusishss@mail.ru>
2023-12-01 11:35:39 +0600

    fill bodies testQuestions for e2e tests

--- test/helpers/inputAndOutputObjects/questionObjects.ts
+++ test/helpers/inputAndOutputObjects/questionObjects.ts
@@ -1,14 +1,124 @@
+//TODO need fix
 export const testQuestions = {
     inputQuestion1: {
         body: 'question1',
-        correctAnswers: ['correct answer for question1', 'ansForQue1']
+        correctAnswers: ['correct answer for question1', 'answer1']
     },
     outputQuestion1Sa: {
         id: expect.any(String),
         body: 'question1',
-        correctAnswers: ['correct answer for question1', 'ansForQue1'],
+        correctAnswers: ['correct answer for question1', 'answer1'],
         published: false,
         createdAt:expect.any(String),
         updatedAt:expect.any(String),
-    }
+    },
+    inputQuestion2: {
+        body: 'question2',
+        correctAnswers: ['correct answer for question2', 'answer2']
+    },
+    outputQuestion2Sa: {
+        id: expect.any(String),
+        body: 'question2',
+        correctAnswers: ['correct answer for question2', 'answer2'],
+        published: false,
+        createdAt:expect.any(String),
+        updatedAt:expect.any(String),
+    },
+    inputQuestion3: {
+        body: 'question3',
+        correctAnswers: ['correct answer for question3', 'answer3']
+    },
+    outputQuestion3Sa: {
+        id: expect.any(String),
+        body: 'question3',
+        correctAnswers: ['correct answer for question3', 'answer3'],
+        published: false,
+        createdAt:expect.any(String),
+        updatedAt:expect.any(String),
+    },
+    inputQuestion4: {
+        body: 'question1',
+        correctAnswers: ['correct answer for question4', 'answer4']
+    },
+    outputQuestion4Sa: {
+        id: expect.any(String),
+        body: 'question1',
+        correctAnswers: ['correct answer for question4', 'answer4'],
+        published: false,
+        createdAt:expect.any(String),
+        updatedAt:expect.any(String),
+    },
+    inputQuestion5: {
+        body: 'question1',
+        correctAnswers: ['correct answer for question5', 'answer5']
+    },
+    outputQuestion5Sa: {
+        id: expect.any(String),
+        body: 'question1',
+        correctAnswers: ['correct answer for question5', 'answer5'],
+        published: false,
+        createdAt:expect.any(String),
+        updatedAt:expect.any(String),
+    },
+    inputQuestion6: {
+        body: 'question1',
+        correctAnswers: ['correct answer for question6', 'answer6']
+    },
+    outputQuestion6Sa: {
+        id: expect.any(String),
+        body: 'question1',
+        correctAnswers: ['correct answer for question6', 'answer6'],
+        published: false,
+        createdAt:expect.any(String),
+        updatedAt:expect.any(String),
+    },
+    inputQuestion7: {
+        body: 'question1',
+        correctAnswers: ['correct answer for question7', 'answer7']
+    },
+    outputQuestion7Sa: {
+        id: expect.any(String),
+        body: 'question1',
+        correctAnswers: ['correct answer for question7', 'answer7'],
+        published: false,
+        createdAt:expect.any(String),
+        updatedAt:expect.any(String),
+    },
+    inputQuestion8: {
+        body: 'question1',
+        correctAnswers: ['correct answer for question8', 'answer8']
+    },
+    outputQuestion8Sa: {
+        id: expect.any(String),
+        body: 'question1',
+        correctAnswers: ['correct answer for question8', 'answer8'],
+        published: false,
+        createdAt:expect.any(String),
+        updatedAt:expect.any(String),
+    },
+    inputQuestion2: {
+        body: 'question1',
+        correctAnswers: ['correct answer for question2', 'answer2']
+    },
+    outputQuestion2Sa: {
+        id: expect.any(String),
+        body: 'question1',
+        correctAnswers: ['correct answer for question2', 'answer2'],
+        published: false,
+        createdAt:expect.any(String),
+        updatedAt:expect.any(String),
+    },
+    inputQuestion2: {
+        body: 'question1',
+        correctAnswers: ['correct answer for question2', 'answer2']
+    },
+    outputQuestion2Sa: {
+        id: expect.any(String),
+        body: 'question1',
+        correctAnswers: ['correct answer for question2', 'answer2'],
+        published: false,
+        createdAt:expect.any(String),
+        updatedAt:expect.any(String),
+    },
+    
 }
