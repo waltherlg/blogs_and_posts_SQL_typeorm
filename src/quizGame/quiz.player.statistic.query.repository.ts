a4bae67713922bf2f251d6a1c677bb7ebafdd7b0
waltherlg <rusishss@mail.ru>
2024-05-02 17:50:23 +0600

    map for req top users

--- src/quizGame/quiz.player.statistic.query.repository.ts
+++ src/quizGame/quiz.player.statistic.query.repository.ts
@@ -20,6 +20,28 @@ export class PlayerStatisticQueryRepository {
     .leftJoin('statistic.Users', 'player')
 
     const statistic = await queryBuilder.getMany()
+    const statisticForOutput = statistic.map((stat) => {
+    return {
+      sumScore: stat.sumScore,
+      avgScores: stat.avgScores,
+      gamesCount: stat.gamesCount,
+      winsCount: stat.winsCount,
+      lossesCount: stat.lossesCount,
+      drawsCount: stat.drawsCount,
+      player: {
+        id: stat.userId,
+        login: stat.Users.login
+      }
+    }
+  })
+    const topPlayers = {
+      pagesCount: 0,
+      page: 0,
+      pageSize: 0,
+      totalCount: 0,
+      items: statisticForOutput
+    }
+    return topPlayers
     
     
   }
