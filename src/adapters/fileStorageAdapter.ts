0793f52f552df0a370a3f5db5adafab699ca093d
waltherlg <rusishss@mail.ru>
2024-06-11 10:14:11 +0600

    added @aws-sdk/client-s3

--- src/adapters/fileStorageAdapter.ts
+++ src/adapters/fileStorageAdapter.ts
@@ -1,4 +1,4 @@
-//import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { Injectable } from '@nestjs/common';
 import * as dotenv from 'dotenv';
 import { CustomisableException } from '../exceptions/custom.exceptions';
@@ -7,50 +7,50 @@ const accessKey = process.env.YANDEX_CLOUD_KEY;
 const kyeId = process.env.YANDEX_CLOUD_KEY_ID;
 const bucketName = process.env.YANDEX_CLOUD_BUCKET_NAME;
 
-// @Injectable()
-// export class S3StorageAdapter {
-//     s3Client: S3Client;
-//     constructor() {
-//         const REGION = 'us-east-1';
-//         this.s3Client = new S3Client({
-//             region: REGION,
-//             endpoint: 'https://storage.yandexcloud.net',
-//             credentials: {
-//                 secretAccessKey: accessKey,
-//                 accessKeyId: kyeId
-//             }
-//         })
-//     }
+@Injectable()
+export class S3StorageAdapter {
+    s3Client: S3Client;
+    constructor() {
+        const REGION = 'us-east-1';
+        this.s3Client = new S3Client({
+            region: REGION,
+            endpoint: 'https://storage.yandexcloud.net',
+            credentials: {
+                secretAccessKey: accessKey,
+                accessKeyId: kyeId
+            }
+        })
+    }
 
-//   private async saveImageFile(
-//     key,
-//     buffer: Buffer
-// ){
-//     const bucketParams = {
-//         Bucket: bucketName,
-//         Key: key,
-//         Body: buffer,
-//         ContentType: 'images/png'
-//     }
+  private async saveImageFile(
+    key,
+    buffer: Buffer
+){
+    const bucketParams = {
+        Bucket: bucketName,
+        Key: key,
+        Body: buffer,
+        ContentType: 'images/png'
+    }
 
-//     const command = new PutObjectCommand(bucketParams)
-//     try {
-//         const uploadResult = await this.s3Client.send(command)
-//         return uploadResult
-//     } catch (error) {
-//         console.log(error);
-//         throw new CustomisableException('uploadFile', 'unable upload this file', 418)
-//     }
+    const command = new PutObjectCommand(bucketParams)
+    try {
+        const uploadResult = await this.s3Client.send(command)
+        return uploadResult
+    } catch (error) {
+        console.log(error);
+        throw new CustomisableException('uploadFile', 'unable upload this file', 418)
+    }
 
-// }
+}
 
-// async saveBlogWallpaper(
-//     userId: string,
-//     blogId: string,
-//     bufer: Buffer,
-// ){
-//     const uploadKey = `content/images/${userId}/blogs/${blogId}_wallpaper.png`
-//     const uploadResult = await this.saveImageFile(uploadKey, bufer)
-//     return uploadResult
-// }
-// }
+async saveBlogWallpaper(
+    userId: string,
+    blogId: string,
+    bufer: Buffer,
+){
+    const uploadKey = `content/images/${userId}/blogs/${blogId}_wallpaper.png`
+    const uploadResult = await this.saveImageFile(uploadKey, bufer)
+    return uploadResult
+}
+}
