b567ec4d61482b1b5e3b36fc8be1cc5363e52412
waltherlg <rusishss@mail.ru>
2024-07-24 09:02:34 +0600

    start create UserSubscribeBlogCase and endpoint

--- src/blogs/api/public.blogs.controller.ts
+++ src/blogs/api/public.blogs.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param, Post, Query, Req, UseGuards } from '@nestjs/common';
 import { BlogsQueryRepository } from '../infrostracture/blogs.query.repository';
 import {
   DEFAULT_BLOGS_QUERY_PARAMS,
@@ -16,6 +16,11 @@ import {
 } from '../../exceptions/custom.exceptions';
 import { IsCustomUrl, StringTrimNotEmpty } from '../../middlewares/validators';
 import { OptionalJwtAuthGuard } from '../../auth/guards/optional-jwt-auth.guard';
+import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
+import { request } from 'express';
+import { CommandBus } from '@nestjs/cqrs';
+import { UserSubscribeBlogCommand } from '../application/use-cases/user-subscribe-blog-use-case';
+import { handleActionResult } from '../../helpers/enum.action.result.helper';
 
 export class CreateBlogInputModelType {
   @StringTrimNotEmpty()
@@ -58,6 +63,7 @@ export class BlogsController {
     private readonly checkService: CheckService,
     private readonly blogsQueryRepository: BlogsQueryRepository,
     private readonly postsQueryRepository: PostsQueryRepository,
+    private readonly commandBus: CommandBus
   ) {}
   @Get()
   async getAllBlogs(@Query() queryParams: RequestBlogsQueryModel) {
@@ -90,5 +96,17 @@ export class BlogsController {
       throw new CustomisableException('blog', 'blog not found', 404);
     }
     return blog;
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post(':blogId/subscription')
+  @HttpCode(204)
+  async subscribeUserToBlog(
+    @Req() request,
+    @Param('blogId') blogId ){
+
+      const result = await this.commandBus.execute(new UserSubscribeBlogCommand(request.user.userId, blogId))
+      handleActionResult(result)
+
   }
 }
---
+++ src/blogs/application/use-cases/user-subscribe-blog-use-case.ts
@@ -0,0 +1,25 @@
+import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
+import { BlogsRepository } from "../../infrostracture/blogs.repository";
+import { TelegramAdapter } from "../../../adapters/telegram.adapter";
+import { ActionResult } from "../../../helpers/enum.action.result.helper";
+
+
+export class UserSubscribeBlogCommand {
+    constructor(
+        public userId: string,
+        public blogId: string
+    ){}
+}
+
+@CommandHandler(UserSubscribeBlogCommand)
+export class UserSubscribeBlogCase implements ICommandHandler<UserSubscribeBlogCommand>{
+    constructor(
+        private readonly blogRepository: BlogsRepository,
+        private readonly telegramAdapter: TelegramAdapter
+    ) {}
+//TODO: UserSubscribeBlogCase
+    async execute(command: UserSubscribeBlogCommand): Promise<ActionResult> {
+        return ActionResult.Success
+    }
+
+}
